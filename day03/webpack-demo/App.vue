37db729c52b0c5082c51fc2457ad7aaae689f59a
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2020-07-02 11:23:00 +0800

    HTML5(vue study day03); study vue.js day03

---
+++ day03/webpack-demo/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="app">
+		<h1>welcome to suzhou</h1>
+		<h2 @click="change">{{name}}</h2>
+		
+		<user-list></user-list>
+	</div>
+</template>
+
+<script>
+	
+	//导入模块
+	import User from './components/User.vue';
+	
+	// console.log(111);
+	export default {
+		data(){
+			return{
+				name: 'tom'
+			}
+		},
+		methods: {
+			change(){
+				this.name = '汤姆';
+			}
+		},
+		components: {
+			'user-list':User
+		}
+	}
+</script>
+
+<style>
+	body {
+		background-color: #ccc;
+	}
+</style>
---
+++ day03/webpack-demo/webpack.config.js
@@ -0,0 +1,40 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+	//配置入口文件
+	entry: './main.js',
+	//配置入口文件输出位置
+	output: {
+		path: __dirname, //项目根路径
+		filename: 'build.js'
+	},
+	//配置模块加载器
+	module: {
+		rules: [{
+				test: /\.vue$/, //所有以.vue结尾的文件都由vue-loader加载
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.js$/, //所有以.js结尾的文件都由babel-loader加载，除了node_modules以外
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}, //配置处理.css文件的第三方处理规则
+			{
+				test: /\.less$/,
+				use: ["style-loader", 'css-loader', 'less-loader']
+			},
+			{
+				test: /\.scss$/,
+				use: ["style-loader", 'css-loader', 'sass-loader']
+			},
+		]
+	},
+
+	plugins: [
+		new VueLoaderPlugin()
+	]
+}
